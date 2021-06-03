dd99cac4ea7dfbac80a4a219a4c8c86a6a6c5e7e
ErikSuca <erik.sucasai@gmail.com>
2021-06-02 22:10:02 -0300

    sendFile funcionando correctamente

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,4 +4,4 @@ const router = express.Router();
 
 router.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'index.html')));
 
-module.exports(router);
+module.exports = router;
