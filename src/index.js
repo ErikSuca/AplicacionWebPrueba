ed8085bcf568e4d550814f8632473f253cc34104
ErikSuca <erik.sucasai@gmail.com>
2021-06-02 22:01:07 -0300

    Base del proyecto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const web = require('./routes/web');
+// server
+app.listen( 3000, ()=> console.log("Server on http://localhost:3000"));
+
+// public static 
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+//websites routes
+app.use(web);
---
+++ src/routes/web.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'index.html')));
+
+module.exports(router);
