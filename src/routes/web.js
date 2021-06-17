2ac97e9226f7cb45aeb930ecc2accc2bf71b1158
ErikSuca <erik.sucasai@gmail.com>
2021-06-16 21:45:28 -0300

    html register junto con la nueva ruta en web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'index.html')));
+router.get('/register.html', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'register.html')));
 
 module.exports = router;
