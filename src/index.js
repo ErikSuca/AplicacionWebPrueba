9cf1f6ecab034fe9410b5cceb68fd448904428d6
ErikSuca <erik.sucasai@gmail.com>
2021-06-23 22:37:35 -0300

    cambie la ruta para heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const web = require('./routes/web');
 // server
-app.listen( 3000, ()=> console.log("Server on http://localhost:3000"));
+app.listen( process.env.PORT || 3000, ()=> console.log("Server on http://localhost:3000"));
 
 // public static 
 app.use(express.static(path.resolve(__dirname, "../public")));
