524ceafacd036c22606d50e00af2cfb8696a813d
ErikSuca <erik.sucasai@gmail.com>
2021-06-23 22:54:15 -0300

    agregue una ruta

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'index.html')));
 router.get('/home.html', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'index.html')));
 router.get('/register.html', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'register.html')));
 router.get('/login.html', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'login.html')));
